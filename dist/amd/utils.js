67e97104f56f55a655b7f5be25bcb94036e0eec7
Marshall Thompson <marshall@creativeideal.net>
2016-07-10 20:27:56 -0600

    0.6.4

---
+++ dist/amd/utils.js
@@ -0,0 +1,9 @@
+/*can-connect-feathers@0.6.3#utils*/
+define(['exports'], function (exports) {
+    'use strict';
+    Object.defineProperty(exports, '__esModule', { value: true });
+    exports.stripSlashes = stripSlashes;
+    function stripSlashes(location) {
+        return location.replace(/^(\/*)|(\/*)$/g, '');
+    }
+});
