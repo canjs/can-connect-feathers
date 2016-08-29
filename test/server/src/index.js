8d19bb93ba80bc00d6fbaba4e70d90a65c32e3dd
Marshall Thompson <marshall@creativeideal.net>
2016-08-29 12:05:42 -0600

    Add server for integration tests with Feathers.

---
+++ test/server/src/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const app = require('./app');
+const port = app.get('port');
+const server = app.listen(port);
+
+server.on('listening', () =>
+  console.log(`Feathers application started on ${app.get('host')}:${port}`)
+);
+
+if (process.argv.indexOf('timeout') >= 0) {
+  setTimeout(function(){
+    server.close();
+    process.exit(0);
+  }, 10000);
+}
---
+++ test/server/src/services/index.js
@@ -0,0 +1,13 @@
+'use strict';
+const messages = require('./messages');
+const authentication = require('./authentication');
+const user = require('./user');
+
+module.exports = function() {
+  const app = this;
+
+
+  app.configure(authentication);
+  app.configure(user);
+  app.configure(messages);
+};
