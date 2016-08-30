485ed845e0074767b20aed7f2935c6f9ee5df07e
Marshall Thompson <marshall@creativeideal.net>
2016-08-29 23:54:39 -0600

    1.1.4

---
+++ dist/cjs/utils.js
@@ -0,0 +1,27 @@
+/*can-connect-feathers@1.1.3#utils*/
+'use strict';
+function stripSlashes(location) {
+    return location.replace(/^(\/*)|(\/*)$/g, '');
+}
+function addAliases(service) {
+    service.find = service.getListData;
+    service.get = service.getData;
+    service.create = service.createData;
+    service.update = service.updateData;
+    service.patch = service.patchData;
+    service.remove = service.destroyData;
+    return service;
+}
+Object.defineProperties(module.exports, {
+    stripSlashes: {
+        get: function () {
+            return stripSlashes;
+        }
+    },
+    addAliases: {
+        get: function () {
+            return addAliases;
+        }
+    },
+    __esModule: { value: true }
+});
