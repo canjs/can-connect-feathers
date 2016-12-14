eccbb2f242874af1f4fbe3c07853c8179e644e53
Marshall Thompson <marshall@creativeideal.net>
2016-12-13 23:15:06 -0700

    Upgrade to use feathers-authentication-client.

--- session/session_test.js
+++ session/session_test.js
@@ -5,7 +5,7 @@ var clearCookies = require('../test/clear-cookies');
 var socketio = require('feathers-socketio/client');
 var rest = require('feathers-rest/client');
 var jQuery = require('jquery');
-var io = require('socket.io-client');
+var io = require('socket.io-client/dist/socket.io');
 
 QUnit.module('can-connect-feathers/session');
 
