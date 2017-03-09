4a64ea992485c498ead9470700d76cb71f05a0d8
Marshall Thompson <marshall@creativeideal.net>
2017-03-08 17:55:57 -0700

    Add core-js shims

--- test/test.js
+++ test/test.js
@@ -1,3 +1,10 @@
+require('core-js/modules/es6.array.from');
+require('core-js/modules/es6.object.assign');
+require('core-js/modules/es6.array.find');
+require('core-js/modules/es6.reflect.construct');
+require('core-js/modules/es6.object.set-prototype-of');
+require('core-js/modules/es6.reflect.set-prototype-of');
+
 require('../service/service_test');
 require('../session/session_test');
 require('../utils/utils_test');
