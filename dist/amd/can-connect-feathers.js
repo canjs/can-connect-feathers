a2053b87697f2d646fe2a3c8db8ef054a36c2f7d
Marshall Thompson <marshall@creativeideal.net>
2017-10-04 11:32:57 -0600

    3.7.0

---
+++ dist/amd/can-connect-feathers.js
@@ -0,0 +1,13 @@
+/*can-connect-feathers@3.6.2#can-connect-feathers*/
+define([
+    'require',
+    'exports',
+    'module',
+    './service/service',
+    './session/session'
+], function (require, exports, module) {
+    module.exports = {
+        service: require('./service/service'),
+        session: require('./session/session')
+    };
+});
