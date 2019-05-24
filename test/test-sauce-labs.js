4a0d1f69622079287b791cad1da702e9b9596d12
Chasen Le Hara <git@chasenlehara.com>
2019-05-24 09:32:08 -0700

    Re-order Sauce Labs browsers to fail faster

--- test/test-sauce-labs.js
+++ test/test-sauce-labs.js
@@ -8,10 +8,20 @@ var commandTimeout = 600; // seconds, default 300, max 600
 var idleTimeout = 1000; // seconds, default 90, max 1000
 
 // https://github.com/SeleniumHQ/selenium/wiki/DesiredCapabilities
-var platforms = [{
-	browserName: 'firefox',
+var platforms = [{/*
+	browserName: 'internet explorer',
 	platform: 'Windows 10',
-	version: 'latest',
+	version: '11.0'
+}, { */
+	browserName: 'MicrosoftEdge',
+	platform: 'Windows 10',
+	maxDuration: maxDuration,
+	commandTimeout: commandTimeout,
+	idleTimeout: idleTimeout
+}, {
+	browserName: 'safari',
+	platform: 'OS X 10.13',
+	version: '11',
 	maxDuration: maxDuration,
 	commandTimeout: commandTimeout,
 	idleTimeout: idleTimeout
@@ -23,19 +33,9 @@ var platforms = [{
 	commandTimeout: commandTimeout,
 	idleTimeout: idleTimeout
 }, {
-	browserName: 'safari',
-	platform: 'OS X 10.12',
+	browserName: 'firefox',
+	platform: 'Windows 10',
 	version: 'latest',
-	maxDuration: maxDuration,
-	commandTimeout: commandTimeout,
-	idleTimeout: idleTimeout
-}, {/*
-	browserName: 'internet explorer',
-	platform: 'Windows 10',
-	version: '11.0'
-}, { */
-	browserName: 'MicrosoftEdge',
-	platform: 'Windows 10',
 	maxDuration: maxDuration,
 	commandTimeout: commandTimeout,
 	idleTimeout: idleTimeout
